12977a82f53b3d87e5110c7ca01d663b16e2b53b
Sreevatsan <123719399+TangerineGlacier@users.noreply.github.com>
2025-01-15 17:33:06 +0530

    added refactoring.nvim

--- init.lua
+++ init.lua
@@ -100,7 +100,24 @@ require("lazy").setup({
   },
 
 { "nvim-treesitter/playground", cmd = "TSPlaygroundToggle" },
-
+{
+		"ThePrimeagen/refactoring.nvim",
+		keys = {
+			{
+				"<leader>r",
+				function()
+					require("refactoring").select_refactor({
+						show_success_message = true,
+					})
+				end,
+				mode = "v",
+				noremap = true,
+				silent = true,
+				expr = false,
+			},
+		},
+		opts = {},
+	},
 	{
 		"nvim-treesitter/nvim-treesitter",
 		opts = {
