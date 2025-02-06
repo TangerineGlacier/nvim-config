df9e9820726505cc3f6891ee7f439bcdcbb07dd4
Sreevatsan <123719399+TangerineGlacier@users.noreply.github.com>
2025-02-06 16:28:35 +0530

    Glacier init.lua

--- ftdetect/init.lua
+++
@@ -1,9 +0,0 @@
--- https://neovim.io/doc/user/lua.html#vim.filetype.add()
-vim.filetype.add {
-	extension = {
-		json = "jsonc",
-		rasi = "rasi",
-		html = "html",
-		zsh = "sh",
-	},
-}
--- lua/config/lualine/components/mode.lua
+++
@@ -1,9 +0,0 @@
-local colors = require("ui.colors")
-local no_seps = require("configs.lualine.seps").no_seps
-
-return {
-	"mode",
-	separator = no_seps,
-	icons_enabled = true,
-	cond = function() return vim.o.columns > 70 end,
-}
