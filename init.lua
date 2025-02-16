6f1e8beba2c5ea437e7cb1be931b7a3dcc088173
Sreevatsan <123719399+TangerineGlacier@users.noreply.github.com>
2025-02-16 23:11:51 +0530

    Shift copy paste movement

--- init.lua
+++ init.lua
@@ -1,5 +1,32 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
+-- Use the system clipboard for all yank, delete, change, and put operations
+vim.opt.clipboard = "unnamedplus"
+
+local opts = { noremap = true, silent = true }
+
+-- Mappings for starting selection with Shift+Arrow keys
+vim.keymap.set("n", "<S-Left>", "v<Left>", opts)
+vim.keymap.set("n", "<S-Right>", "v<Right>", opts)
+vim.keymap.set("n", "<S-Up>", "v<Up>", opts)
+vim.keymap.set("n", "<S-Down>", "v<Down>", opts)
+
+-- In Visual mode: if you press Shift+Arrow, extend the selection.
+vim.keymap.set("v", "<S-Left>", "<Left>", opts)
+vim.keymap.set("v", "<S-Right>", "<Right>", opts)
+vim.keymap.set("v", "<S-Up>", "<Up>", opts)
+vim.keymap.set("v", "<S-Down>", "<Down>", opts)
+
+-- In Visual mode: if you press an arrow key WITHOUT Shift, cancel Visual mode then move.
+vim.keymap.set("v", "<Left>", "<Esc><Left>", opts)
+vim.keymap.set("v", "<Right>", "<Esc><Right>", opts)
+vim.keymap.set("v", "<Up>", "<Esc><Up>", opts)
+vim.keymap.set("v", "<Down>", "<Esc><Down>", opts)
+
+-- In Visual mode: map Ctrl+C to copy selection to the clipboard.
+vim.keymap.set("v", "<C-c>", '"+y', opts)
+
+
 require('keymaps')
 require('plugins.lazy')
 require('plugins.misc')
