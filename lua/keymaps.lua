f438564aaef9dca101c3098b361de492d56849f6
Sreevatsan <123719399+TangerineGlacier@users.noreply.github.com>
2025-03-15 21:42:19 +0530

    Update keymaps.lua

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,40 +1,36 @@
+-- CMD key shortcuts for text editing
 vim.api.nvim_set_keymap("i", "jj", "<Esc>", {noremap=false})
--- twilight
+-- Twilight
 vim.api.nvim_set_keymap("n", "tw", ":Twilight<enter>", {noremap=false})
--- buffers
+-- Buffers
 vim.api.nvim_set_keymap("n", "tk", ":blast<enter>", {noremap=false})
 vim.api.nvim_set_keymap("n", "tj", ":bfirst<enter>", {noremap=false})
 vim.api.nvim_set_keymap("n", "th", ":bprev<enter>", {noremap=false})
 vim.api.nvim_set_keymap("n", "tl", ":bnext<enter>", {noremap=false})
 vim.api.nvim_set_keymap("n", "td", ":bdelete<enter>", {noremap=false})
--- files
+-- Files
 vim.api.nvim_set_keymap("n", "QQ", ":q!<enter>", {noremap=false})
 vim.api.nvim_set_keymap("n", "WW", ":w!<enter>", {noremap=false})
 vim.api.nvim_set_keymap("n", "E", "$", {noremap=false})
 vim.api.nvim_set_keymap("n", "B", "^", {noremap=false})
 vim.api.nvim_set_keymap("n", "TT", ":TransparentToggle<CR>", {noremap=true})
 vim.api.nvim_set_keymap("n", "ss", ":noh<CR>", {noremap=true})
---
--- splits
---
+
+-- Splits
 vim.api.nvim_set_keymap("n", "<C-W>,", ":vertical resize -10<CR>", {noremap=true})
 vim.api.nvim_set_keymap("n", "<C-W>.", ":vertical resize +10<CR>", {noremap=true})
 vim.keymap.set('n', '<space><space>', "<cmd>set nohlsearch<CR>")
+
 -- Quicker close split
-vim.keymap.set("n", "<leader>qq", ":q<CR>",
-  {silent = true, noremap = true}
-)
--- split terminal 
+vim.keymap.set("n", "<leader>qq", ":q<CR>", {silent = true, noremap = true})
 
-local terminal_buf = nil  -- Store terminal buffer ID
-
+-- Terminal Split
+local terminal_buf = nil
 function ToggleTerminal()
   if terminal_buf and vim.api.nvim_buf_is_valid(terminal_buf) then
-    -- If terminal is open, close it
     vim.api.nvim_command("bd! " .. terminal_buf)
     terminal_buf = nil
   else
-    -- If terminal is closed, reopen it
     vim.api.nvim_command("belowright split | terminal")
     terminal_buf = vim.api.nvim_get_current_buf()
   end
@@ -42,10 +38,10 @@ end
 
 function NewTerminal()
   vim.api.nvim_command("belowright split | terminal")
-  terminal_buf = vim.api.nvim_get_current_buf()  -- Update to last opened terminal
+  terminal_buf = vim.api.nvim_get_current_buf()
 end
 
--- Keymaps
+-- Terminal Keymaps
 vim.api.nvim_set_keymap('n', '<C-`>', ':lua ToggleTerminal()<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('t', '<C-`>', '<C-\\><C-n>:lua ToggleTerminal()<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<C-S-`>', ':lua NewTerminal()<CR>', { noremap = true, silent = true })
@@ -55,13 +51,30 @@ vim.api.nvim_set_keymap('t', '<C-S-`>', '<C-\\><C-n>:lua NewTerminal()<CR>', { n
 vim.keymap.set("n", "<C-S-Right>", ":bnext<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<C-S-Left>", ":bprevious<CR>", { noremap = true, silent = true })
 
+-- CMD Key Navigation
+vim.api.nvim_set_keymap("n", "<D-Up>", "gg", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<D-Down>", "G", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<D-Left>", "^", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<D-Right>", "$", { noremap = true, silent = true })
 
+-- CMD+A for Select All
+vim.api.nvim_set_keymap("n", "<D-a>", "ggVG", { noremap = true, silent = true })
 
--- Keymaps for better default experience
--- See `:help vim.keymap.set()`
+-- CMD+C to Copy
+vim.api.nvim_set_keymap("v", "<D-c>", "\"+y", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<D-c>", "V\"+y", { noremap = true, silent = true })
+
+-- CMD+X to Cut
+vim.api.nvim_set_keymap("v", "<D-x>", "\"+d", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<D-x>", "V\"+d", { noremap = true, silent = true })
+
+-- CMD+V to Paste
+vim.api.nvim_set_keymap("n", "<D-v>", "\"+p", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("i", "<D-v>", "<C-r>+", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "<D-v>", "\"+p", { noremap = true, silent = true })
+
+-- Better defaults
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
-
--- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
@@ -74,7 +87,10 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.keymap.set("n", "<leader>e", "<cmd>GoIfErr<cr>", { silent = true, noremap = true })
   end,
 })
--- vim.keymap.set("n", "<D-p>", ":NvimTreeToggle<CR>", { silent = true, noremap = true })
+
+-- File Explorer
 vim.keymap.set("n", "<D-p>", ":Telescope find_files<CR>", { silent = true, noremap = true })
+vim.keymap.set("n", "<D-S-P>", "<cmd>Telescope live_grep<CR>", { noremap = true, silent = true })
+
 
 vim.keymap.set('n', '<leader>l', ':Lazy<CR>', { noremap = true, silent = true })
