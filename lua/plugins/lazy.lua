c3cbbfc61ea8dd8b9324a22b0d69929408788082
Sreevatsan <123719399+TangerineGlacier@users.noreply.github.com>
2025-04-10 11:36:51 +0530

    harpoon configured

--- lua/plugins/lazy.lua
+++ lua/plugins/lazy.lua
@@ -127,8 +127,26 @@ require('lazy').setup({
   {
     "ThePrimeagen/harpoon",
     branch = "harpoon2",
-    dependencies = { "nvim-lua/plenary.nvim" }
-},
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      local harpoon = require("harpoon")
+      harpoon:setup()
+
+      -- Basic keybindings
+      vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end, { desc = "Add file to harpoon" })
+      vim.keymap.set("n", "<leader>h", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "Open harpoon menu" })
+
+      -- Quick access to marked files
+      vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end, { desc = "Go to harpoon file 1" })
+      vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end, { desc = "Go to harpoon file 2" })
+      vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end, { desc = "Go to harpoon file 3" })
+      vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end, { desc = "Go to harpoon file 4" })
+
+      -- Navigation between marked files
+      vim.keymap.set("n", "<leader>n", function() harpoon:list():next() end, { desc = "Go to next harpoon file" })
+      vim.keymap.set("n", "<leader>p", function() harpoon:list():prev() end, { desc = "Go to previous harpoon file" })
+    end,
+  },
   {
     "lervag/vimtex",
     ft = { "tex" },
